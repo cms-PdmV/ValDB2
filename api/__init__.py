9b68ddbf4df552f8c5e12723488576296cf1bb6e
chanchana <north1602@gmail.com>
2021-07-13 16:33:32 +0700

    Remove todo module

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,5 @@
 from flask_restx import Api
 
-from .todo import api as todo_namespace
 from .campaign import api as campaign_namespace
 from .report import api as report_namespace
 from .group import api as group_namespace
@@ -12,7 +11,6 @@ api = Api(
     description='A description',
 )
 
-api.add_namespace(todo_namespace, path='/api/todo')
 api.add_namespace(campaign_namespace, path='/api/campaigns')
 api.add_namespace(report_namespace, path='/api/reports')
 api.add_namespace(group_namespace, path='/api/groups')
--- models/todo.py
+++
@@ -1,5 +0,0 @@
-from core import Model
-
-class Todo(Model):
-    name: str
-    is_done: bool
