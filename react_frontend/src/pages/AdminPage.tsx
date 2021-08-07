a75a3ebaed3cd5cd1cac045f541a8d61f44ab9f2
chanchana <north1602@gmail.com>
2021-08-07 16:31:14 +0700

    Rename admin page

--- react_frontend/src/pages/AdminPage.tsx
+++ react_frontend/src/pages/AdminPage.tsx
@@ -13,15 +13,11 @@ export function AdminPage(): ReactElement {
       <Paper>
         <List component="nav" aria-label="secondary mailbox folders">
           <ListItem onClick={() => history.push('/admin/users')} button>
-            <ListItemText primary="User Management" />
+            <ListItemText primary="All User List" />
             <ListItemSecondaryAction><FontAwesomeIcon icon={faChevronRight}/></ListItemSecondaryAction>
           </ListItem>
           <ListItem onClick={() => history.push('/admin/usergroups')} button>
             <ListItemText primary="User Permission Group Management" />
-            <ListItemSecondaryAction><FontAwesomeIcon icon={faChevronRight}/></ListItemSecondaryAction>
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="Campaign Category/Permission Group" />
             <ListItemSecondaryAction><FontAwesomeIcon icon={faChevronRight}/></ListItemSecondaryAction>
           </ListItem>
         </List>
