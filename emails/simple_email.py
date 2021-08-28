a3a8b3be9ae7ca052e7acc595c7c8c2b19731d05
chanchana <north1602@gmail.com>
2021-08-28 19:53:21 +0700

    Fix path

--- emails/simple_email.py
+++ emails/simple_email.py
@@ -1,8 +1,8 @@
 from emails.template import EmailTemplate, render_template
 
-SIMPLE_TEMPLATE = './templates/simple_template.html'
+SIMPLE_TEMPLATE = 'emails/templates/simple_template.html'
 
-class SimpleEmailTempalte(EmailTemplate):
+class SimpleEmailTemplate(EmailTemplate):
     def build(self, content='', subject=''):
         self.subject = f'[TEST] {subject}'
         self.recipients = ['wic.chanchana@gmail.com']
