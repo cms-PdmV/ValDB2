cbbd8042ee3fc38438474cd8d37d5ca3e561e4b4
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2023-03-02 23:29:09 +0100

    Gunicorn: Load environment variables from file

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,10 @@
+import dotenv
 import multiprocessing
 import os
+
+# Load environment variables
+# from .env file
+dotenv.load_dotenv()
 
 host = os.environ.get('HOST', '0.0.0.0')
 port = os.environ.get('PORT', '8003')
