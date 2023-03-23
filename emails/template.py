3473b7dd7682aa21471bbeab1c4a645c7b3b008d
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2023-03-23 13:33:41 +0100

    For development, send emails only to pdmvserv@cern.ch email account

--- emails/template.py
+++ emails/template.py
@@ -1,41 +1,51 @@
-'''
+"""
 Email template base model
-'''
+"""
+import os
 from services.email_service import EmailService
 
+
 class EmailAddress:
-    '''
+    """
     Common email address
-    '''
-    forum = 'hn-cms-relval@cern.ch'
+    """
+
+    enable_to_production = os.getenv("PRODUCTION")
+    cms_talk = "cmstalk+relval@dovecotmta.cern.ch"
+    dev_forum = "pdmvserv@cern.ch"
+    forum = cms_talk if enable_to_production else dev_forum
+
 
 def format_new_line(html):
-    '''
+    """
     Convert \n to <br>
-    '''
-    return html.replace('\n', '<br>')
+    """
+    return html.replace("\n", "<br>")
+
 
 def render_template(template_path, **kwargs):
-    '''
+    """
     Render HTML email template and fill information
-    '''
-    with open(template_path, encoding='utf-8') as file:
+    """
+    with open(template_path, encoding="utf-8") as file:
         html_template = file.read()
         for key, value in kwargs.items():
-            html_template = html_template.replace(f'{{{{{key}}}}}', value)
+            html_template = html_template.replace(f"{{{{{key}}}}}", value)
         return html_template
 
+
 class EmailTemplate:
-    '''
+    """
     Base email template
-    '''
-    def __init__(self, subject='', body='', recipients='') -> None:
+    """
+
+    def __init__(self, subject="", body="", recipients="") -> None:
         self.subject = subject
         self.body = body
         self.recipients = recipients
 
     def send(self):
-        '''
+        """
         Send email via email service
-        '''
+        """
         EmailService.send(self.subject, self.body, self.recipients)
--- services/email_service.py
+++ services/email_service.py
@@ -6,10 +6,12 @@ from email.message import EmailMessage
 import logging
 import smtplib
 
-_logger = logging.getLogger('service.email')
-enable_to_production = os.getenv('PRODUCTION')
+_logger = logging.getLogger("service.email")
+enable_to_production = os.getenv("PRODUCTION")
+dev_recipients = ["pdmvserv@cern.ch"]
 
-class EmailService():
+
+class EmailService:
     """
     Email service
     """
@@ -20,17 +22,21 @@ class EmailService():
         Send email
         """
         if not enable_to_production:
-            return
+            _logger.warning("[DEV] Original recipients: %s", recipients)
+            _logger.warning("[DEV] Override recipients to: %s", dev_recipients)
+            recipients = dev_recipients
+            subject = f"[DEV] {subject}"
+
         message = EmailMessage()
         body = body.strip()
-        message.set_content(body, subtype='html')
-        message['Subject'] = subject
-        message['From'] = 'PdmV Service Account <pdmvserv@cern.ch>'
-        message['To'] = ', '.join(recipients)
-        message['Cc'] = 'pdmvserv@cern.ch'
+        message.set_content(body, subtype="html")
+        message["Subject"] = subject
+        message["From"] = "PdmV Service Account <pdmvserv@cern.ch>"
+        message["To"] = ", ".join(recipients)
+        message["Cc"] = "pdmvserv@cern.ch"
 
         smtp = smtplib.SMTP()
         smtp.connect()
-        _logger.info('Sending email %s to %s', message['Subject'], message['To'])
+        _logger.info("Sending email %s to %s", message["Subject"], message["To"])
         smtp.send_message(message)
         smtp.quit()
