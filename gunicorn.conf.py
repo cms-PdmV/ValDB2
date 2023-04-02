901b420c55211be62ef8e65a00a573bb4312f6a2
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2023-04-02 14:34:53 +0200

    Set default number of Gunicorn workers to 1

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,11 @@
 import dotenv
-import multiprocessing
 import os
 
 # Load environment variables
 # from .env file
 dotenv.load_dotenv()
 
-host = os.environ.get('HOST', '0.0.0.0')
-port = os.environ.get('PORT', '8003')
+host = os.environ.get("HOST", "0.0.0.0")
+port = os.environ.get("PORT", "8003")
 bind = f"{host}:{port}"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
