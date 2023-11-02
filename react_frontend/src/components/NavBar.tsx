1d35adf07f09ffd6a735a336683d5e47f6973bb2
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2023-11-02 16:28:36 +0100

    Remove `<Button>` href properties
    1. Include child `<Link>` tag to include this functionality and avoid
    redirecting to wrong locations.

--- react_frontend/src/components/NavBar.tsx
+++ react_frontend/src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import { Avatar, Box, Button } from '@material-ui/core';
-import { boxShadow, primaryColor } from '../utils/css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFeather } from '@fortawesome/free-solid-svg-icons';
 import { ReactElement } from 'react';
 import { useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
 import { User, UserRole } from '../types';
+import { boxShadow, primaryColor } from '../utils/css';
 
 interface NavBarProp {
   user?: User
@@ -30,53 +31,61 @@ export function NavBar(prop: NavBarProp): ReactElement {
   return (
     <Box height="64px" boxShadow={boxShadow} display="flex" alignItems="center" padding="0 1rem" style={{background: '#ffffff'}}>
       {/* Home button and its icon */}
-      <Button href={homeLink}>
+      <Button>
         <Box color={primaryColor} fontWeight={800} fontSize="1.5rem">
-          <FontAwesomeIcon icon={faFeather} />&nbsp;&nbsp;ValDB
+          <Link to={homeLink} style={{ textDecoration: 'none' }}>
+            <FontAwesomeIcon icon={faFeather} />&nbsp;&nbsp;ValDB
+          </Link>
         </Box>
       </Button>
       {/* Navigation buttons at left side */}
       { prop.user && <Box marginLeft="auto" display="flex">
         {/* Campaigns button */}
         <Button
-          href={campaignsLink}
           style={currentPath === 'campaigns' ? selectedPathStyle : {}}
         >
-          Campaigns
+          <Link to={campaignsLink} style={{ textDecoration: 'none' }}>
+            Campaigns
+          </Link>
         </Button>
         <Box width="1rem" />
         {/* Validator button */}
         { prop.user.role === UserRole.VALIDATOR && <>
           <Button
-            href={reportsLink}
             style={currentPath === 'reports' ? selectedPathStyle : {}}
           >
-            My Reports
+            <Link to={reportsLink} style={{ textDecoration: 'none' }}>
+              My Reports
+            </Link>
           </Button>
           <Box width="1rem" />
         </>}
         {/* Administrator button */}
         { prop.user.role === UserRole.ADMIN && <>
           <Button
-            href={adminLink}
             style={currentPath === 'admin' ? selectedPathStyle : {}}
           >
-            Administator
+            <Link to={adminLink} style={{ textDecoration: 'none' }}>
+              Administator
+            </Link>
           </Button>
           <Box width="1rem" />
         </>}
         {/* Contact button */}
         <Button
-          href={contactLink}
           style={currentPath === 'contact' ? selectedPathStyle : {}}
         >
-          Contact Us
+          <Link to={contactLink} style={{ textDecoration: 'none' }}>
+            Contact Us
+          </Link>
         </Button>
         <Box width="1rem" />
         {/* User button */}
-        <Button href={userLink} style={currentPath === 'user' ? selectedPathStyle : {}}>
-          {prop.user.fullname}&nbsp;&nbsp;
-          <Avatar style={{width: '32px', height: '32px'}}>{prop.user.fullname[0] || ' '}</Avatar>
+        <Button style={currentPath === 'user' ? selectedPathStyle : {}}>
+          <Link to={userLink} className="inherit" style={{ textDecoration: 'none' }}>
+            {prop.user.fullname}&nbsp;&nbsp;
+            <Avatar style={{width: '32px', height: '32px'}}>{prop.user.fullname[0] || ' '}</Avatar>
+          </Link>
         </Button>
       </Box>}
     </Box>
